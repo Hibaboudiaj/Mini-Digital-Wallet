88629c5d0ecf1763c4c8b7f3802058bb20e657e8
Hibaboudiaj <boudiajhiba2@gmail.com>
2026-02-23 13:01:00 +0100

    done

--- routes.js
+++ routes.js
@@ -1,13 +1,4 @@
 // routes.js
-
-const {
-  createUser,
-  getUsers,
-} = require("./controllers/userController");
-
-const {
-  createWallet,
-  deposit,// routes.js
 // This file reads the URL and calls the right function
 const {
   createUser,
@@ -43,37 +34,6 @@ function handleRoutes(req, res, body) {
   }
   
   // No route matched
-  res.writeHead(404, { "Content-Type": "application/json" });
-  res.end(JSON.stringify({ message: "Route not found" }));
-}
-
-module.exports = handleRoutes;
-
-  withdraw,
-} = require("./controllers/walletController");
-
-function handleRoutes(req, res, body) {
-
-  if (req.method === "POST" && req.url === "/users") {
-    return createUser(req, res, body);
-  }
-
-  if (req.method === "GET" && req.url === "/users") {
-    return getUsers(req, res);
-  }
-
-  if (req.method === "POST" && req.url === "/wallets") {
-    return createWallet(req, res, body);
-  }
-
-  if (req.method === "POST" && req.url === "/deposit") {
-    return deposit(req, res, body);
-  }
-
-  if (req.method === "POST" && req.url === "/withdraw") {
-    return withdraw(req, res, body);
-  }
-
   res.writeHead(404, { "Content-Type": "application/json" });
   res.end(JSON.stringify({ message: "Route not found" }));
 }
--- server.js
+++ server.js
@@ -1,18 +1,7 @@
-const http = require("http");
-
-const handleRoutes = require("./routes");
-
-// const { log } = require("console");
-
-const server = http.createServer((req, res) => {
-  let body = "";
-
-  req.on("data", (chunk) => {
-    body += chunk.toString();
-  });//server.js
+//server.js
 //This is the entry point - it starts the server and receives requests
-
 const http = require("http");
+
 const handleRoutes = require("./routes");
 // const { log } = require("console");
 
@@ -28,20 +17,6 @@ const server = http.createServer((req, res) => {
   req.on("end", () => {
   const parsedBody = body ? JSON.parse(body) : {};
   handleRoutes(req, res, parsedBody);
-  });
-});
-server.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
-});
-
-
-  req.on("end", () => {
-    if (body) {
-      body = JSON.parse(body);
-    } else {
-      body = {};
-    }
-    handleRoutes(req, res, body);
   });
 });
 server.listen(3000, () => {
